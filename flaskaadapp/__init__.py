e8b9b1b604009cafd20f115bfdb0b656980e3202
KC Munnings <kc@kcmunnings.com>
2019-05-16 19:57:07 -0400

    updatetd PREFERRED_URL_SCHEME

--- flaskaadapp/__init__.py
+++ flaskaadapp/__init__.py
@@ -9,3 +9,4 @@ app = Flask(__name__)
 app.secret_key = 'SUPER_SECRET_KEY'
 app.config['AZURE_OAUTH_CLIENT_ID'] = os.environ['AAD_CLIENTID']
 app.config['AZURE_OAUTH_CLIENT_SECRET'] = os.environ['AAD_CLIENTSECRET']
+app.config['PREFERRED_URL_SCHEME'] = 'https'
