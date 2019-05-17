1e3f13b75e19e445e0323503cf7b186b8a6ba296
KC Munnings <kc@kcmunnings.com>
2019-05-17 10:36:31 -0400

    changed preferred URL scheme

--- flaskaadapp/__init__.py
+++ flaskaadapp/__init__.py
@@ -11,7 +11,7 @@ import os
 app = Flask(__name__)
 
 # App Proxy
-app.wsgi_app = ProxyFix(app.wsgi_app,x_for=1,x_proto=1,x_host=1,x_port=1,x_prefix=1)
+app.wsgi_app = ProxyFix(app.wsgi_app,x_for=1,x_proto=1,x_host=1,x_port=1,x_prefix=1) # Had to set these numbers to make auth work in Linux App Service!!!!
 
 # App Secret
 app.secret_key = 'SUPER_SECRET_KEY'
@@ -19,4 +19,4 @@ app.secret_key = 'SUPER_SECRET_KEY'
 # App Config
 app.config['AZURE_OAUTH_CLIENT_ID'] = os.environ['AAD_CLIENTID']
 app.config['AZURE_OAUTH_CLIENT_SECRET'] = os.environ['AAD_CLIENTSECRET']
-app.config['PREFERRED_URL_SCHEME'] = 'https'
+# app.config['PREFERRED_URL_SCHEME'] = 'https'
